acbcc4cadcd87b96179002945cf9d8b442c1f5bd
jhr88 <uchung22@gmail.com>
2025-12-23 15:20:13 +0900

    1223 python

---
+++ 02code/01test/test01.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+# age = 100
+# print(age)
+# print(type(age))
+
+
+x = [1,2,3,4]
+y = [10,20,30,40]
+
+
+# plt.plot(x,y)
+# plt.bar(x,y)
+plt.barh(x,y)
+plt.show()
